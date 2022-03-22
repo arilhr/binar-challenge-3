a6a0fd168f256c8b7e3e7893f1ea42d7d910bc79
arilhr <aurielharlih9213@gmail.com>
2022-03-22 18:54:24 +0700

    add login form

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LoginForm } from "../assets/components/LoginForm";
+
+export const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <h3>Welcome, Admin BCR</h3>
+      <LoginForm />
+    </div>
+  );
+};
