86923c451c43295f940fdec589952c5cedd646d8
arilhr <aurielharlih9213@gmail.com>
2022-03-23 14:46:02 +0700

    add search bar

--- src/assets/components/Navbar/NavbarDefault.jsx
+++ src/assets/components/Navbar/NavbarDefault.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./NavbarDefault.scss";
 import { Container, Navbar } from "react-bootstrap";
 import Logo from "../../img/binar-logo.png";
+import { SearchBar } from "../SearchBar/SearchBar";
 
 export const NavbarDefault = () => {
   return (
     <Navbar>
       <Container>
-        <Navbar.Brand href="#home">
-          BCR
-        </Navbar.Brand>
+        <Navbar.Brand href="#home">BCR</Navbar.Brand>
+        <SearchBar />
       </Container>
     </Navbar>
   );
